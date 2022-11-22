6beaa77541ac0fe8a3278653416e6ab696d6f74e
a37591234 <a37591234@gmail.com>
2022-11-22 16:17:57 +0800

    11/22 01

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src="" alt="" />
+        <div className="user">
+          <img src="" alt="" />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>123</h1>
+        <p>456</p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Write = () => {
-  return <div>Write</div>;
+  return <div className="write"></div>;
 };
 
 export default Write;
