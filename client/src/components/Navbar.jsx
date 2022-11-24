72bea0f3df6e87a13f7952bb341c82d18d9ff93e
a37591234 <a37591234@gmail.com>
2022-11-24 16:42:57 +0800

    11/24 01

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { React, useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../img/logo.png";
+import { AuthContext } from "../context/authContext.js";
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="container">
@@ -27,8 +29,14 @@ const Navbar = () => {
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           <span className="write">
             <Link className="link" to="/write">
               Write
